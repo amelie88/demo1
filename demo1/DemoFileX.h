bd26b870ae74f91dd45d947a25c991f5908dea48
Sjostrand Gereholt Amelie <sjostrandgereholtamelie@Sjostrand-Gereholt-Amelies-MacBook-Pro.local>
2013-04-05 11:33:46 +0200

    Added DemoFilex

---
+++ demo1/DemoFileX.h
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.h
+//  demo1
+//
+//  Created by Sjostrand Gereholt Amelie on 2013-04-05.
+//  Copyright (c) 2013 Sjostrand Gereholt Amelie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFileX : NSObject
+
+@end
---
+++ demo1/DemoFileX.m
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.m
+//  demo1
+//
+//  Created by Sjostrand Gereholt Amelie on 2013-04-05.
+//  Copyright (c) 2013 Sjostrand Gereholt Amelie. All rights reserved.
+//
+
+#import "DemoFileX.h"
+
+@implementation DemoFileX
+
+@end
