68cfd903d75e860ef906790e75170b6b106112c2
Sjostrand Gereholt Amelie <sjostrandgereholtamelie@Sjostrand-Gereholt-Amelies-MacBook-Pro.local>
2013-04-05 11:21:05 +0200

    Initial Commit

---
+++ demo1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  demo1
+//
+//  Created by Sjostrand Gereholt Amelie on 2013-04-05.
+//  Copyright (c) 2013 Sjostrand Gereholt Amelie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
